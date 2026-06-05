00e3986121aedcd2b242efeedb79dbe3ca2f6da8
Luis <lfsarmiento@uniboyaca.edu.co>
2026-06-04 22:06:20 -0500

    Luis:Creación de endpoints health check en microservicios

---
+++ ProyectoFinal_Backend/Backend/ms-auth/public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+
+$app->get('/health', function (Request $request, Response $response) {
+    $data = [
+        'status' => true,
+        'microservice' => 'ms-auth',
+        'message' => 'Microservicio de autenticacion funcionando correctamente'
+    ];
+
+    $response->getBody()->write(json_encode($data));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
